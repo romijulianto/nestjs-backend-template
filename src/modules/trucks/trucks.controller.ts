a8182a21db34cddbae0d1cc60eb9b59a2fe1d63e
romijulianto <romyjulians@gmail.com>
2024-02-17 16:11:41 +0700

    feat: add crud detil

--- src/modules/trucks/trucks.controller.ts
+++ src/modules/trucks/trucks.controller.ts
@@ -20,22 +20,45 @@ import {
 export class TrucksController {
   constructor(private readonly trucksService: TrucksService) {}
 
-  @Get(':plat')
+  @Get('')
   @ApiOperation({
-    summary: 'Get history location trukc by plat and time',
+    summary: 'Get history location truck by plat and time',
     description: 'Get history truck',
   })
   @ApiOkResponse({ type: [TruckEntity] })
   async findTruckLocation(
-    @Param('plat') plat: string,
+    @Query('plat') plat: string,
     @Query('startDate') start: Date,
     @Query('endDate') end: Date,
   ): Promise<ApiResponse<any>> {
     try {
       const data = (
-        await this.trucksService.findTruckLocation(plat, start, end)
+        await this.trucksService.getTruckLocation(plat, start, end)
       ).map((truckData) => new TruckEntity(truckData));
       if (!data || data.length === 0) {
+        throw new NotFoundException(
+          `${ApiResponseCustomMessage.VEHICLE_NOT_FOUND} ${plat} ${start} ${end}`,
+        ).getResponse();
+      }
+      return new ApiResponse(HttpStatus.OK, 'success', data);
+    } catch (error) {
+      throw new BadGatewayException();
+    }
+  }
+
+  @Get('/detail')
+  @ApiOperation({
+    summary: 'Get detil truck by plat',
+    description: 'Get detil truck',
+  })
+  @ApiOkResponse()
+  async findTruckDetail(
+    @Query('plat') plat: string,
+  ): Promise<ApiResponse<any>> {
+    try {
+      const data = await this.trucksService.getTruckDetail(plat);
+
+      if (!data) {
         throw new NotFoundException(
           `${ApiResponseCustomMessage.VEHICLE_NOT_FOUND} ${plat}`,
         ).getResponse();
--- src/modules/trucks/trucks.service.ts
+++ src/modules/trucks/trucks.service.ts
@@ -5,11 +5,15 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class TrucksService {
   constructor(private prisma: PrismaService) {}
 
-  async findTruckLocation(plat: string, start: Date, end: Date) {
+  async getTruckLocation(plat: string, start: Date, end: Date) {
     return await this.prisma.mtGpsLastLocation.findMany({
       where: { PLATENO: plat, INSERT_DATE: { gte: start, lte: end } },
     });
   }
 
-
+  async getTruckDetail(plat: string) {
+    return await this.prisma.mmsVehicle.findFirst({
+      where: { idTruck: plat },
+    });
+  }
 }
